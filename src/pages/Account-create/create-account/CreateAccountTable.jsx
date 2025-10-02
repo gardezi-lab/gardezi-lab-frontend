51c4d64bf199aad27a4d8a32f0d4736b9cf5fa74
Muhammad Mazhar <contactmazhar88@gmail.com>
2025-10-02 13:07:04 +0500

    account api binding

--- src/pages/Account-create/create-account/CreateAccountTable.jsx
+++ src/pages/Account-create/create-account/CreateAccountTable.jsx
@@ -1,4 +1,5 @@
 import { ThreeCircles } from "react-loader-spinner";
+import { FaRegTrashCan, FaPenToSquare } from "react-icons/fa6";
 
 export default function CreateAccountTable({ departmentList, onDelete, onEdit, loading }) {
   return (
@@ -13,6 +14,10 @@ export default function CreateAccountTable({ departmentList, onDelete, onEdit, l
               <tr style={{ backgroundColor: "#1c2765", color: "white" }}>
                 <th scope="col">Sr.</th>
                 <th scope="col">Name</th>
+                <th scope="col">Code</th>
+                <th scope="col">Opening Balence</th>
+                <th scope="col">Parent Account</th>
+                <th scope="col">Opening Balence Date</th>
                 <th scope="col">Action</th>
               </tr>
             </thead>
@@ -43,10 +48,14 @@ export default function CreateAccountTable({ departmentList, onDelete, onEdit, l
             ) : departmentList?.length > 0 ? (
               <tbody>
                 {departmentList.map((dept, index) => (
-                  <tr key={dept.department_id}>
+                  <tr key={dept.id}>
                     <td>{index + 1}</td>
+                    <td>{dept.head_name}</td>
                     <td>{dept.head_code}</td>
-                    <td>
+                    <td>{dept.ob}</td>
+                    <td>{dept.parent_account}</td>
+                    <td>{dept.ob_date}</td>
+                    {/* <td>
                       <div className="d-flex gap-3 align-items-center justify-content-center">
 
                         <button
@@ -63,6 +72,19 @@ export default function CreateAccountTable({ departmentList, onDelete, onEdit, l
                         >
                           <i className="fas fa-trash-alt" style={{ fontSize: "20px", cursor: "pointer" }}></i>
                         </button>
+                      </div>
+                    </td> */}
+                     <td>
+                      <div className="d-flex gap-2 align-items-center justify-content-center">
+                          <FaPenToSquare  
+                          onClick={() => onEdit(dept)} style={{ fontSize: "22px", cursor: "pointer" }} />
+                        <FaRegTrashCan onClick={() => {
+                          if (window.confirm("Are you sure you want to delete this department?")) {
+                            onDelete(dept.id);
+                          }
+                        }} 
+                         style={{ fontSize: "22px", cursor: "pointer", color:'red' }}
+                        />
                       </div>
                     </td>
                   </tr>
